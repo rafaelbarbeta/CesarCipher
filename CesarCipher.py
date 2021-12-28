8d32cd240ee73382fb1b96b97e71b488de704dba
Rafael Barbeta <rafa.barbeta@gmail.com>
2021-12-28 13:28:24 -0300

    add list comprehension to alphabet list

--- CesarCipher.py
+++ CesarCipher.py
@@ -1,6 +1,6 @@
 
 def Cesar(text, num):
-    alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    alphabet = [chr(i) for i in range(97,123)]
     text_converted_list = []
     text_converted = ""
     was_uppercase = False
